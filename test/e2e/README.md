c0bf06edb2b9b9747ef8851cb2ba7a259ffb6a5c
Tomer Eliyahu <tomer.b.eliyahu@intel.com>
2020-11-05 12:41:23 +0200

    test: add e2e testing support
    Add e2e testing support, including a script which downloads the test
    suite, generates configuration file and runs the tests.
    
    Based on https://kubernetes.io/blog/2020/01/08/testing-of-csi-drivers/
    and
    https://github.com/kubernetes/kubernetes/blob/master/test/e2e/storage/external/README.md
    
    See test/e2e/README.md for more details.
    
    Signed-off-by: Tomer Eliyahu <tomer.b.eliyahu@intel.com>

---
+++ test/e2e/README.md
@@ -0,0 +1,69 @@
+# e2e testing
+
+Helper script to run e2e testing on kubernetes cluster running Lightbits CSI plugin, based on [Testing of CSI drivers blog post](https://kubernetes.io/blog/2020/01/08/testing-of-csi-drivers/).
+
+The script downloads the test suite (e2e and ginkgo) from https://dl.k8s.io/<k8s version>/kubernetes-test-linux-amd64.tar.gz, generates a storage-class.yaml and a test-driver.yaml based on either environment variables (see below) or optional parameters to set the Lightbits paramters, then runs the tests.
+
+There is only one mandatory variable which is the mgmt-endpoints which connects the generated storage-class to the Lightbits storage cluster.
+Set this variable using either `LB_CSI_SC_MGMT_ENDPOINT` environment variable or pass `-m|--mgmt-endpoint` to the script.
+
+Other optional variables include the replication factor (default 3) and compression (default disabled).
+
+The lightbits CSI plugin currently supports only the following capabilities described in [3]:
+
+ - exec
+ - persistence
+ - controllerExpansion
+ - nodeExpansion
+
+These are hardcoded in the script, once a new capability is supported, this script needs to be updated.
+
+# Running the tests
+
+To run tests: `./test.sh -v --kubeconfig </path/to/config> -m <endpoints> -r <replication> -t <test>`
+
+Examples:
+
+Run all tests
+```
+./test.sh -v --kubeconfig ~/.kube/config -m "10.10.10.51:80,10.10.10.52:80,10.10.10.53:80" -r 3 -t all
+```
+
+Run all block tests
+```
+./test.sh -v --kubeconfig=~/.kube/config -m "10.10.10.51:80,10.10.10.52:80,10.10.10.53:80" -r 3 -t block
+```
+
+Run all volume expand tests
+```
+./test.sh -v --kubeconfig=~/.kube/config -m "10.10.10.51:80,10.10.10.52:80,10.10.10.53:80" -r 3 -t resize
+```
+
+# Running specific test suites not covered by the script
+
+By default, the script runs all test suites by providing `csi.lightbitslabs.com` to the `ginkgo.focus` argument.
+If you wish to run all testscases from a specific test suite, you should first run the script with the `--skip-test` argument so it will download the test binaries and generate the test yamls.
+Then, to run a specific test suite, first find out the name of the suite you want to run from [kubernetes/test/e2e/storage/testsuites/base.go](https://github.com/kubernetes/kubernetes/blob/6d01c5a58996d1619ac049c2b3077274299eb2d0/test/e2e/storage/testsuites/base.go#L76).
+For example, the volume expand test suite name is `volume-expand` and defined in `initVolumeExpandTestSuite()` function in [kubernetes/test/e2e/storage/testsuites/volume_expand.go](https://github.com/kubernetes/kubernetes/blob/6d01c5a58996d1619ac049c2b3077274299eb2d0/test/e2e/storage/testsuites/volume_expand.go#L62).
+
+In order to run all tests in that suite, run:
+
+```
+KUBECONFIG="$KUBECONFIG" ./e2e.test -ginkgo.v -ginkgo.focus='csi.lightbitslabs.com.*volume-expand' -report-dir=/tmp/ -storage.testdriver=./test-driver.yaml
+```
+
+In order to run a specific test case, add it to the ginkgo focus regex. For example in order to run ["should resize volume when PVC is edited while pod is using it"](https://github.com/kubernetes/kubernetes/blob/6d01c5a58996d1619ac049c2b3077274299eb2d0/test/e2e/storage/testsuites/volume_expand.go#L238), run:
+
+```
+KUBECONFIG="$KUBECONFIG" ./e2e.test -ginkgo.v -ginkgo.focus='csi.lightbitslabs.com.*should.resize.volume.when.PVC.is.edited.while.pod.is.using.it' -report-dir=/tmp/ -storage.testdriver=./test-driver.yaml
+```
+
+# References
+
+[1] https://kubernetes.io/blog/2020/01/08/testing-of-csi-drivers/
+
+[2] https://github.com/kubernetes/kubernetes/blob/master/test/e2e/storage/external/README.md
+
+[3] https://github.com/kubernetes/kubernetes/blob/master/test/e2e/storage/testsuites/testdriver.go
+
+[4] https://github.com/kubernetes/kubernetes/blob/master/test/e2e/storage/external/external.go
---
+++ test/e2e/test.sh
@@ -0,0 +1,182 @@
+#!/bin/bash
+# Copyright (C) 2020 Intel Corporation
+# SPDX-License-Identifier: Apache-2.0
+
+SCRIPTPATH="$( cd "$(dirname "$0")" >/dev/null 2>&1 ; pwd -P )"
+
+dbg() {
+    if [ "$VERBOSE" = "true" ]; then echo "$(basename "$0"): $*"; fi
+}
+
+status() {
+    printf '\033[1;35m%s\033[0m\n' "$*"
+}
+
+err() {
+    printf '\033[1;31m%s\033[0m\n' "$*"
+}
+
+info() {
+    printf '\033[1;35m%s: %s\033[0m\n' "$(basename "$0")" "$*"
+}
+
+success() {
+    printf '\033[1;32m%s\033[0m\n' "$*"
+}
+
+run() {
+    echo "$*"
+    "$@" || exit $?
+}
+
+test() {
+    local focus
+    local cmd
+
+    info "Start $TEST tests"
+    case "$TEST" in
+        resize) focus='csi.lightbitslabs.com.*volume-expand' ;;
+        *) focus='csi.lightbitslabs.com' ;;
+    esac
+
+    cmd="KUBECONFIG=\"$KUBECONFIG\" ./e2e.test -ginkgo.v -ginkgo.skip='Disruptive' -ginkgo.reportPassed -ginkgo.focus='${focus}' -storage.testdriver=\"$TESTDIR\"/test-driver.yaml -report-dir=\"$TESTDIR\" | tee test.log"
+    dbg "$cmd"
+    (cd $TESTDIR && eval $cmd && sed -i -r 's/\x1B\[([0-9]{1,3}(;[0-9]{1,2})?)?[mGK]//g' test.log)
+
+    info "Done"
+}
+
+download() {
+    local test_archive_url="https://dl.k8s.io/$CLUSTER_VERSION/kubernetes-test-linux-amd64.tar.gz"
+    
+    mkdir -p $TESTDIR
+    [ -e "$TESTDIR"/e2e.test -a -e "$TESTDIR"/ginkgo ] && {
+        dbg "Found e2e.test and ginkgo in $TESTDIR, skip download"
+        return 0
+    }
+
+    info "Downloading kubernetes/test/bin/e2e.test and kubernetes/test/bin/ginkgo from $test_archive_url to $TESTDIR"
+    (cd $TESTDIR && curl --location "$test_archive_url" | tar --strip-components=3 -zxf - kubernetes/test/bin/e2e.test kubernetes/test/bin/ginkgo)
+    info "Done"
+}
+
+generate() {
+    cat > $TESTDIR/storage-class.yaml <<EOF
+kind: StorageClass
+apiVersion: storage.k8s.io/v1
+metadata:
+  name: lb-csi-sc
+provisioner: csi.lightbitslabs.com
+allowVolumeExpansion: true
+parameters:
+  mgmt-endpoint: $MGMT_ENDPOINT
+  replica-count: "$REPLICA_COUNT"
+  compression: $COMPRESSION
+EOF
+    cat > $TESTDIR/test-driver.yaml <<EOF
+StorageClass:
+  FromFile: $TESTDIR/storage-class.yaml
+SnapshotClass:
+  FromName: true
+DriverInfo:
+  Name: csi.lightbitslabs.com
+  Capabilities:
+    persistence: true
+    exec: true
+    controllerExpansion: true
+    nodeExpansion: true
+EOF
+    dbg "Generated $TESTDIR/storage-class.yaml:"
+    [ "$VERBOSE" = "true" ] && cat $TESTDIR/storage-class.yaml
+    dbg "Generated $TESTDIR/test-driver.yaml:"
+    [ "$VERBOSE" = "true" ] && cat $TESTDIR/test-driver.yaml
+}
+
+usage() {
+    echo "usage: $(basename "${0}") [-hv] [-k path_to_kubeconfig] [-m mgmt-endpoint] [-r replica-count] [-c enable-compression] [-s|--skip-test]"
+    echo "  options:"
+    echo "      -h|--help - show this help menu"
+    echo "      -v|--verbose - verbosity on"
+    echo "      -k|--kubeconfig - path to kubeconfig, default is KUBECONFIG environment variable"
+    echo "      -m|--mgmt-endpoint - mgmt-endpoint parameter for the storage class. Default is using LB_CSI_SC_MGMT_ENDPOINT environment variable"
+    echo "      -r|--replica-count - replica-count parameter for the storage class. Default is using LB_CSI_SC_REPLICA_COUNT environment variable OR 3 if not defined"
+    echo "      -c|--enable-compression - compression parameter for the storage class. Default is using LB_CSI_SC_COMPRESSION environment variable OR disabled if not defined"
+    echo "      -s|--skip-test - only download and generate the test files without running the test"
+    echo "      -t|--test - run a specific test. Supported tests - ${SUPPORTED_TESTS[@]} (default all)"
+    echo "      -d|--test-dir - test path, where all artifacts will be stored. (default $TESTDIR)"
+}
+
+main() {
+    if ! OPTS=$(getopt -o 'hvk:m:r:cst:d:' --long help,verbose,kubeconfig:,mgmt-endpoint:,replica-count:,compression,skip-test,test:,test-dir: -n 'parse-options' -- "$@"); then
+        err "Failed parsing options." >&2 ; usage; exit 1 ;
+    fi
+
+    eval set -- "$OPTS"
+
+    while true; do
+        case "$1" in
+            -v | --verbose)       VERBOSE=true; shift ;;
+            -h | --help)          usage; exit 0; shift ;;
+            -k | --kubeconfig)    KUBECONFIG="$2"; shift; shift ;;
+            -m | --mgmt-endpoint) MGMT_ENDPOINT="$2"; shift; shift ;;
+            -r | --replica-count) REPLICA_COUNT="$2"; shift; shift ;;
+            -c | --compression)   COMPRESSION=enabled; shift ;;
+            -s | --skip-test)     SKIP_TEST=true; shift ;;
+            -t | --test)          TEST="$2"; shift; shift ;;
+            -d | --test-dir)      TESTDIR="$(readlink -f $2)"; shift; shift ;;
+            -- ) shift; break ;;
+            * ) err "unsupported argument $1"; usage; exit 1 ;;
+        esac
+    done
+
+    if [ -z "$KUBECONFIG" ]; then
+        err "KUBECONFIG environment variable not set and -k|--kubeconfig option not used"
+        usage
+        exit 1
+    fi
+
+    if [ -z "$MGMT_ENDPOINT" ]; then
+        err "LB_CSI_SC_MGMT_ENDPOINT environment variable not set and -m|--mgmt-endpoint variable not used"
+        usage
+        exit 1
+    fi
+
+    CLUSTER_VERSION=$(kubectl --kubeconfig $KUBECONFIG version --short | grep Server | awk '{print $3}')
+    if [ -z "$CLUSTER_VERSION" ]; then
+        err "Failed to get k8s cluster version using kubectl --kubeconfig $KUBECONFIG version --short"
+        usage
+        exit 1
+    fi
+
+    if [[ ! " ${SUPPORTED_TESTS[@]} " =~ " ${TEST} " ]]; then
+        err "Unsupported test ${TEST}"
+        usage
+        exit 1
+    fi
+    [ -z "$TESTDIR" ] && TESTDIR="$SCRIPTPATH"/results/"$CLUSTER_VERSION"/"$TEST"
+
+    dbg "TEST=$TEST"
+    dbg "TESTDIR=$TESTDIR"
+    dbg "KUBECONFIG=$KUBECONFIG"
+    dbg "CLUSTER_VERSION=$CLUSTER_VERSION"
+    dbg "MGMT_ENDPOINT=$MGMT_ENDPOINT"
+    dbg "REPLICA_COUNT=$REPLICA_COUNT"
+    dbg "COMPRESSION=$COMPRESSION"
+
+    download
+    generate
+    [ "$SKIP_TEST" = "false" ] && test
+}
+
+VERBOSE=false
+CLUSTER_VERSION=
+TESTDIR=
+SKIP_TEST=false
+SUPPORTED_TESTS=(all resize)
+TEST=all
+# storage class lb specific params
+MGMT_ENDPOINT="$LB_CSI_SC_MGMT_ENDPOINT"
+REPLICA_COUNT="${LB_CSI_SC_REPLICA_COUNT-3}"
+COMPRESSION="${LB_CSI_SC_COMPRESSION-disabled}"
+
+main "$@"
