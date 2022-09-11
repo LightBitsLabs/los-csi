9ff0e4512755611d4c14f9955ebb89853a4bab6c
Yogev Cohen <yogev@lightbitslabs.com>
2022-09-11 13:11:17 +0300

    pkg/util/strlist: add Contains and Remove str slice utility functions
    issue: LBM1-12623

--- pkg/util/strlist/strlist.go
+++ pkg/util/strlist/strlist.go
@@ -35,3 +35,21 @@ func CopyUniqueSorted(s []string) []string {
 	sort.Strings(res)
 	return res
 }
+
+func Contains(s []string, e string) bool {
+	for _, a := range s {
+		if a == e {
+			return true
+		}
+	}
+	return false
+}
+
+func Remove(s []string, r string) []string {
+	for i, v := range s {
+		if v == r {
+			return append(s[:i], s[i+1:]...)
+		}
+	}
+	return s
+}
